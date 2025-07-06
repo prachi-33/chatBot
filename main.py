6c039e365651a4547b0168ab1f2a97983af847e0
prachi-33 <prachisaran5@gmail.com>
2025-07-06 21:14:08 +0530

    file renamed

--- main.py
+++ main.py
@@ -125,5 +125,5 @@ if __name__ == "__main__":
     import uvicorn
 
     port = int(os.environ.get("PORT", 8000))  
-    uvicorn.run("aiBot:app", host="127.0.0.1", port=port, reload=False)
+    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=False)
 
